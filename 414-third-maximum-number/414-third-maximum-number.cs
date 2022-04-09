991a05fed00a82b05a15ec586bd7e9e4aff37dee
pavneet singh <pavneet2010@gmail.com>
2022-04-09 23:44:55 +0530

    Time: 168 ms (9.36%), Space: 38.5 MB (34.25%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.cs
+++ 414-third-maximum-number/414-third-maximum-number.cs
@@ -1,57 +1,8 @@
 public class Solution {
     public int ThirdMax(int[] nums) {
-         if(nums.Length == 1)
-        {
-            return nums[0];
-        }
-        
-        if(nums.Length == 2)
-        {
-           return nums[0] > nums[1] ? nums[0] : nums[1];
-        }
-        
-        
-        var i = 0;
-        var j = 1;   
-        
-        while(i <= 3)
-        {
-            if(j > nums.Length - 1)
-            {
-                i++;
-                
-                if (i == nums.Length - 1)
-                {
-                    break;
-                }
-                
-                j = i + 1;
-            }
-            
-            if(nums[i] < nums[j])
-            {
-                var t = nums[i];
-                nums[i] = nums[j];
-                nums[j] = t;
-            }
-            
-            j++;
-        }
-        
-        var hashSet = new HashSet<int>(3);
-        var count = 3;
-        
-        for(var k = 0; k < count; k++)
-        {
-            var result = hashSet.Add(nums[k]);
-            
-            if(!result && nums.Length > count)
-            {
-                count++;
-            }
-        }
-        
-        return hashSet.Count >= 3 ? hashSet.Min() : hashSet.Max();
+        var hs = new HashSet<int>(nums);
+            if (hs.Count < 3) return hs.Max();
+            return hs.OrderByDescending(n => n).Take(3).Last();
         
     }
 }
