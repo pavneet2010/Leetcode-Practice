35c665ba76359c5500a015f95a9a41f5988c7018
pavneet singh <pavneet2010@gmail.com>
2022-04-10 20:13:24 +0530

    Time: 92 ms (93.42%), Space: 42.2 MB (11.84%) - LeetHub

---
+++ 563-binary-tree-tilt/563-binary-tree-tilt.cs
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    private int result=0;
+    private int adds=0;
+    public int sum(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        else{
+           return root.val+sum(root.left)+sum(root.right);
+        }
+        
+    }
+    public int FindTilt(TreeNode root) {
+     if(root==null) return 0;
+        
+       int ltsum= FindTilt(root.left);        
+       int rtsum= FindTilt(root.right);
+        if(root.left==null&& root.right==null){            
+            return 0;
+        }
+        else{
+          result+= Math.Abs(sum(root.left)-sum(root.right));            
+        }
+        
+        return Math.Abs(result);
+    }
+}
