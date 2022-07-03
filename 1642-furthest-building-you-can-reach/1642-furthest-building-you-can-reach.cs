529524d6fec32794b04d5f67a0e5e48793bbca40
pavneet singh <pavneet2010@gmail.com>
2022-07-03 19:25:43 +0530

    Time: 353 ms (41.43%), Space: 48.3 MB (43.86%) - LeetHub

---
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cs
@@ -0,0 +1,22 @@
+public class Solution {
+   public int FurthestBuilding(int[] heights, int bricks, int ladders)
+    {
+        PriorityQueue<int, int> priorityQueue = new();
+        for (int index = 0; index < heights.Length - 1; index++)
+        {
+            int heightDifference = heights[index + 1] - heights[index];
+            if (heightDifference <= 0) continue;
+            bricks -= heightDifference;
+            priorityQueue.Enqueue(heightDifference, -heightDifference);
+
+            if (bricks < 0)
+            {
+                if (ladders == 0) return index;
+                bricks += priorityQueue.Dequeue();
+                ladders--;
+            }
+        }
+
+        return heights.Length - 1;
+    }
+}
