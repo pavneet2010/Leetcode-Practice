c9b9d4c711cda6ba18cc9f85652979cdf96fce75
pavneet singh <pavneet2010@gmail.com>
2022-07-03 14:18:29 +0530

    Time: 614 ms (32.69%), Space: 54.6 MB (59.61%) - LeetHub

---
+++ 1836-remove-duplicates-from-an-unsorted-linked-list/1836-remove-duplicates-from-an-unsorted-linked-list.cs
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode DeleteDuplicatesUnsorted(ListNode head) {
+        if(head==null){
+            return head;
+        }
+        Dictionary<int,bool> dict=new Dictionary<int,bool>();
+//         for(ListNode iNode = head; iNode!=null; iNode = head.next) {
+//             if(!dict.ContainsKey(iNode.val)) {
+//                 dict.Add(iNode.val,false);
+//             }else{
+//                 dict[iNode.val]=true;
+//             }
+            
+//         }
+        ListNode temp=head;
+         while(temp!=null){
+            if(!dict.ContainsKey(temp.val))
+                dict.Add(temp.val, false);
+            else
+                dict[temp.val]=true;
+            temp=temp.next;
+        }
+        ListNode result=new ListNode();
+        //dict.Remove(k=>k.Value==true);
+         temp= new ListNode(-1, head);
+         result=temp;
+        while(temp.next!=null){
+            if(dict[temp.next.val])
+                temp.next=temp.next.next;
+            else
+                temp=temp.next;
+        }
+        
+        return result.next;
+        
+    }
+}
