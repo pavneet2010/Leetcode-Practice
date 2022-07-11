c85fcc2d8c90e555da7cebc3c21c208a9b42b68e
pavneet singh <pavneet2010@gmail.com>
2022-07-11 20:26:36 +0530

    Time: 863 ms (83.38%), Space: 131 MB (7.04%) - LeetHub

---
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cs
@@ -0,0 +1,33 @@
+public class TimeMap {
+
+    Dictionary<string, List<(int, string)>> dict = new Dictionary<string, List<(int, string)>>();
+    //Dictionary<string,List<int,string>>() dict=new ();
+    public TimeMap() {
+        
+    }
+    
+    public void Set(string key, string value, int timestamp) {
+        if (!dict.ContainsKey(key))        {
+            
+            dict.Add(key, new List<(int, string)> { (timestamp, value) });
+        }
+        else
+        {
+            dict.TryGetValue(key, out var list);
+            list.Add((timestamp, value));
+        }        
+    }
+    
+    public string Get(string key, int timestamp) {
+        Dictionary<string, string> res = new Dictionary<string, string>();
+        if(dict.ContainsKey(key)){
+            string result=dict[key].LastOrDefault(k=>k.Item1<=timestamp).Item2;
+        if(result==null){
+            return "";
+        }
+        return  result;
+        }
+        return"";
+        
+    }
+}
