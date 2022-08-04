c08b4aee155769dccec2a5a94174f1d629f9653a
pavneet singh <pavneet2010@gmail.com>
2022-08-04 16:01:55 +0530

    Time: 160 ms (83.78%), Space: 43.7 MB (18.58%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cs
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cs
@@ -7,13 +7,10 @@ public class Solution {
             firstList.Add(nums[i]);
             secondList.Add(nums[n+i]);
         }
-//         for(int i=n;i<nums.Length;i++){
-//             secondList.Add(nums[i]);
-//         }
         
-        int length=firstList.Count()>secondList.Count()?firstList.Count():secondList.Count();
+        //int length=firstList.Count()>secondList.Count()?firstList.Count():secondList.Count();
         
-        for(int j=0;j<length;j++){            
+        for(int j=0;j<firstList.Count();j++){            
              resultList.Add(firstList[j]);
              resultList.Add(secondList[j]);
         }
